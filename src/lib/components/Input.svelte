74658409a963626e9e8841b86123c9fae7e4e76a
Samuel Aditia <samueladitia95@gmail.com>
2023-07-28 02:28:29 +0700

    add input component

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let name: string;
+	export let value: string;
+	export let placeholder: string;
+	export let label: string;
+	export let type: string = 'text';
+</script>
+
+<div class="p-2 bg-gray-200 rounded">
+	<div class="text-sm">{label}</div>
+	<input {type} {name} {value} {placeholder} class="bg-gray-200 w-full outline-none" />
+</div>
--- src/routes/(welcome)/login/+page.svelte
+++ src/routes/(welcome)/login/+page.svelte
@@ -1,15 +1,26 @@
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
+</script>
+
 <div class="container flex min-h-screen">
-	<div class="m-auto">
+	<div class="m-auto w-full">
 		<p class="text-center text-3xl">
 			<strong>B&W</strong>
 			International
 		</p>
 
-		<p class="text-center pt-6">Admin sign in</p>
+		<p class="text-center py-6">Admin sign in</p>
 
-		<form class="w-full">
-			<input placeholder="email" type="email" class="w-full" />
-			<input placeholder="passsword" type="password" class="w-full" />
+		<form class="w-full flex flex-col gap-8">
+			<Input name="email" placeholder="email" value="john@mail.com" label="Email" type="email" />
+
+			<Input
+				name="passsword"
+				placeholder="password"
+				value="Password"
+				label="Password"
+				type="password"
+			/>
 			<button>Login </button>
 		</form>
 	</div>
