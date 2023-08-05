358dcf69c0aef93fc588d77d1f0506dec46d01ce
Samuel Aditia <samueladitia95@gmail.com>
2023-08-06 01:16:54 +0700

    add 2 new pages

---
+++ src/routes/(app)/events/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { pb } from "$lib/pocketbase";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		console.log(pb.authStore.isValid, "isvalid");
+	});
+</script>
+
+<h1 class="text-3xl font-bold underline">THIS IS EVENT PAGE</h1>
